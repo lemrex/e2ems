20e8b3fb50849595c3b43b02db841b423e6c500c
Raphael <95745431+lemrex@users.noreply.github.com>
2026-04-14 17:01:22 +0100

    Update API endpoints for authentication and transactions

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,8 @@
 // lib/api.js
-export const TRANSACTION_API = "http://localhost:8000/api";
+// export const TRANSACTION_API = "http://localhost:8000/api";
 
-export const AUTH_API = "http://localhost:5000/api";
+// export const AUTH_API = "http://localhost:5000/api";
 
+export const AUTH_API = "http://e2ems-authentication-test-deployment.apps.hw-ocp4.converge.com:5000/api";
 
+export const TRANSACTION_API = "http://e2ems-transaction-test-deployment.apps.hw-ocp4.converge.com:5000/api";
