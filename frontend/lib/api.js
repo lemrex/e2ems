0aa0b7a70aaec39e12fa1ca9235504535442a231
Raphael <95745431+lemrex@users.noreply.github.com>
2026-04-15 15:33:36 +0100

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -3,6 +3,11 @@
 
 // export const AUTH_API = "http://localhost:5000/api";
 
-export const AUTH_API = "http://e2ems-authentication-test-deployment.apps.hw-ocp4.converge.com:5000/api";
+// export const AUTH_API = "http://e2ems-authentication-test-deployment.apps.hw-ocp4.converge.com:5000/api";
 
-export const TRANSACTION_API = "http://e2ems-transaction-test-deployment.apps.hw-ocp4.converge.com:5000/api";
+// export const TRANSACTION_API = "http://e2ems-transaction-test-deployment.apps.hw-ocp4.converge.com:5000/api";
+
+
+export const AUTH_API = "auth.default.svc.cluster.local:3000/api";
+
+export const TRANSACTION_API = "transaction.default.svc.cluster.local:3000/api";
